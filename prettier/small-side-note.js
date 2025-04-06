7d9af3eb89c49cea16f705b7d5e37943a52f7a1e
1OTH3R <growlingtiger75@gmail.com>
2025-04-06 15:15:34 -0500

    Definitely did not add an easter egg. Maybe.

--- prettier/small-side-note.js
+++ prettier/small-side-note.js
@@ -1,2 +1,7 @@
-// I know it's ironic that this is not very pretty code. It is just for optimization tho.
-print("hi")
+/*
+  I know it's ironic that the back-end code here is not very pretty code.
+  It's just for optimization tho, so it shouldn't matter too much.
+  But at that point in time, am I just wasting some poor person's computer space by trying to make a joke here...?
+  Welp. I don't know.
+  Enjoy this anyways ¯\_(ツ)_/¯
+*/
